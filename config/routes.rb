546ce88b2787358625b238ebd2e8eeb66a1a9132
jolak5 <kayodej4@gmail.com>
2023-07-04 16:14:59 +0100

    style the page further

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   
-  resources :groups do
-    resources :entities
+  resources :groups, only: [:index, :new, :show] do
+    resources :entities, only: [:index, :new, :show] 
   end
   
   resources :users
