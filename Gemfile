e66ac302f1d98d86afcb7d04a67755d83067f776
jolak5 <kayodej4@gmail.com>
2023-07-04 15:44:42 +0100

    install rspec

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ gem 'devise'
 gem 'cancancan'
 
 gem 'erb-formatter'
+
+gem 'rspec'
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :entities
+  has_one_attached :icon
 end
