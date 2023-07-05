aa6b3f7e3090b743ac48cefeb29f5719f53e304b
jolak5 <kayodej4@gmail.com>
2023-07-05 21:55:21 +0100

    fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
+ 
     return unless user.present?
 
     can :read, :all
