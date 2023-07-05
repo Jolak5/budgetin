070a271a2a49a82ba9551036132064230df20d8c
jolak5 <kayodej4@gmail.com>
2023-07-05 14:39:58 +0100

    model for group works

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :entities
   has_one_attached :icon
+
+  validates :name, presence: true, length: { maximum: 250 }
+
 end
--- spec/model/group_spec.rb
+++ spec/model/group_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  subject { Group.new(user_id: 1, name: 'kayode', icon: "default_icon.jpg") }
+  let(:icon_file) { fixture_file_upload(Rails.root.join('app', 'assets', 'images', 'icon.jpg'), 'image/jpeg') }
+  subject { Group.new(user_id: 1, name: 'kayode', icon: icon_file) }
 
   before { subject.save }
 
-  it 'new group should be saved in the database' do
+  it 'attaches the icon file' do
+    expect(subject.icon).to be_attached
+  end
+
+  it 'new post should be saved in the database' do
     expect(subject.new_record?).to be_truthy
   end
 
+  it 'validates the presence of the icon file' do
+    expect(subject).to validate_presence_of(:icon)
+  end
 
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 
 end
