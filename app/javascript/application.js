d651ddb00380abc4c0436b7a3bb1ddeb22bcceb9
jolak5 <kayodej4@gmail.com>
2023-07-06 13:09:50 +0100

    update condtionals

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,22 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburgerMenu = document.querySelector(".hamburger-menu");
-  const bars = document.querySelectorAll(".bar");
-  var navMenu = document.querySelector(".nav-menu");
-
-  hamburgerMenu.addEventListener("click", function () {
-    this.classList.toggle("open");
-    navMenu.classList.toggle("open");
-  });
-
-  const navLinks = document.querySelectorAll(".nav-list a");
-  navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
-      hamburgerMenu.classList.remove("open");
-      navMenu.classList.remove("open");
-    });
-  });
-});
